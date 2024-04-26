bebc2c254d4a2e843998c51c61cca27e5ae56b3c
Espen Moe-Nilssen <espen@medialog.no>
2024-04-26 13:45:46 +0200

    Added control panel setting

--- src/medialog/dropdownmenu/interfaces.py
+++ src/medialog/dropdownmenu/interfaces.py
@@ -2,7 +2,39 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from medialog.controlpanel.interfaces import IMedialogControlpanelSettingsProvider
+
+from zope import schema
+from zope.interface import alsoProvides
+from plone.supermodel import model
+ 
+
 
 
 class IMedialogDropdownmenuLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+    
+
+
+class ISettings(model.Schema):
+    """Adds settings to medialog.controlpanel
+    """
+
+    model.fieldset(
+        'dropdown_menu',
+        label=(u'Dropdown Menu'),
+        fields=[
+            'scale', 
+            ],
+        )
+
+     
+    scale =  schema.Choice(
+        title="scale", 
+        vocabulary='plone.app.vocabularies.ImagesScales',
+        default='icon',
+    )
+
+
+
+alsoProvides(ISettings, IMedialogControlpanelSettingsProvider)
--- src/medialog/dropdownmenu/viewlets/drop_down_menu_viewlet.py
+++ src/medialog/dropdownmenu/viewlets/drop_down_menu_viewlet.py
@@ -2,22 +2,25 @@
 
 from plone.app.layout.viewlets import ViewletBase
 from plone.app.layout.viewlets.common import GlobalSectionsViewlet
+from zope.interface import Interface
 
-# class DropDownMenuViewlet(ViewletBase):
+ 
 
-#     def update(self):
-#         self.message = self.get_message()
+#from plone.registry.interfaces import IRegistry
 
-#     def get_message(self):
-#         return u'My message'
-
-#     def index(self):
-#         return super(DropDownMenuViewlet, self).render()
+from plone import api
 
 
 
+ 
 class DropDownMenuViewlet(GlobalSectionsViewlet):
     
+    def thumb_size(self):
+        thumb_size = api.portal.get_registry_record('medialog.dropdownmenu.interfaces.ISettings.scale', default='icon')
+        #thumb_size = 'icon'
+        return thumb_size
+    
+
     def customize_tab(self, entry, tab):
         """Helper to add custom entry keys/values."""
         #Maybe add option for 'top folder too?
@@ -27,7 +30,7 @@ class DropDownMenuViewlet(GlobalSectionsViewlet):
         """Helper to add custom entry keys/values."""
         entry['image_thumb'] = ''
         if brain.getIcon:
-            entry['image_thumb'] = '<img  alt="" class="subnmenu_image" src="{url}/@@images/image/{thumb}" /> '.format( url = entry['url'], thumb = 'listing' )
+            entry['image_thumb'] = '<img  alt="" class="subnmenu_image" src="{url}/@@images/image/{thumb}" /> '.format( url = entry['url'], thumb = self.thumb_size() )
             
     
     _item_markup_template = (
