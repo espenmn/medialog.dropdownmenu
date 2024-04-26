9e19b74b92b947dc1095eca773e65f10763cca8a
Espen Moe-Nilssen <espen@medialog.no>
2024-04-26 13:49:08 +0200

    Update drop_down_menu_viewlet.py

--- src/medialog/dropdownmenu/viewlets/drop_down_menu_viewlet.py
+++ src/medialog/dropdownmenu/viewlets/drop_down_menu_viewlet.py
@@ -2,24 +2,16 @@
 
 from plone.app.layout.viewlets import ViewletBase
 from plone.app.layout.viewlets.common import GlobalSectionsViewlet
-from zope.interface import Interface
-
- 
-
+#from zope.interface import Interface
 #from plone.registry.interfaces import IRegistry
 
 from plone import api
-
-
-
  
 class DropDownMenuViewlet(GlobalSectionsViewlet):
     
     def thumb_size(self):
         thumb_size = api.portal.get_registry_record('medialog.dropdownmenu.interfaces.ISettings.scale', default='icon')
-        #thumb_size = 'icon'
         return thumb_size
-    
 
     def customize_tab(self, entry, tab):
         """Helper to add custom entry keys/values."""
@@ -31,8 +23,7 @@ class DropDownMenuViewlet(GlobalSectionsViewlet):
         entry['image_thumb'] = ''
         if brain.getIcon:
             entry['image_thumb'] = '<img  alt="" class="subnmenu_image" src="{url}/@@images/image/{thumb}" /> '.format( url = entry['url'], thumb = self.thumb_size() )
-            
-    
+               
     _item_markup_template = (
             '<li class="{id}{has_sub_class} nav-item">'
             '<a href="{url}" class="state-{review_state} nav-link"{aria_haspopup}>{image_thumb}{title}</a>{opener}'   
